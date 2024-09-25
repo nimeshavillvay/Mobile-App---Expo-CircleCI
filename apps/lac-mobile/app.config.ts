95ce4f9888492265987f6753a0450a5f0df0ccb1
nimeshavillvay <nimesha@villvay.com>
2024-09-25 15:50:00 +0530

    trigger the build

--- apps/lac-mobile/app.config.ts
+++ apps/lac-mobile/app.config.ts
@@ -59,7 +59,7 @@ const config: ExpoConfig = {
   owner: "wurth-louis",
   extra: {
     eas: {
-      projectId: "87690a65-51ba-4b7d-878a-8a68f5d7c285",
+      projectId: "efdab9ef-df32-4334-bf22-81f690f9f083",
     },
   },
 };
